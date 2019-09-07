246085574a898f1eb1af11952cd49fb15d52c6dc
chandlerlei2017 <chandlerlei87@gmail.com>
2019-09-07 12:26:18 -0400

    spacing

--- src/components/Track.js
+++ src/components/Track.js
@@ -7,10 +7,14 @@ class Track extends React.Component {
         <div className='col-sm-3'>
           <a href={this.props.track.play} target='_blank' className='s-link' rel='noopener noreferrer'>{this.props.num + ': ' + this.props.track.name}</a>
         </div>
-        <div className='col-sm-3'>
-          <img src={this.props.track.image} alt={this.props.track.album + 'name'} className='track-image rounded'></img>
-          {' '}
-          {this.props.track.album}
+        <div className='col-sm-3 row'>
+          <div className='col-sm-2'>
+            <img src={this.props.track.image} alt={this.props.track.album + 'name'} className='track-image rounded'></img>
+            {' '}
+          </div>
+          <div className='col-sm-10'>
+            {this.props.track.album}
+          </div>
         </div>
         <div className='col-sm-3'>{this.props.track.artists.join(', ')}</div>
       </React.Fragment>
